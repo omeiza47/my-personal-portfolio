7f9011115e05b6408db46973ea6fb5c5bef155b5
omeiza47 <lawalomeiza1@gmail.com>
2024-02-09 15:31:02 +0100

    my personal portfolio commit

---
+++ assets/js/data.js
@@ -0,0 +1,17 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['Hello, I am Lawal Omeiza, a frontend developer and instructor from Nigeria, now exploring the world as a digital nomad and freelancing from various locations globally. My commitment is to elevate your business online by providing top-notch, industry-standard web solutions', 'My expertise spans HTML, CSS, JavaScript, and Git, with a keen focus on expanding my skills in ReactJS. As I continue to deepen my knowledge in ReactJS', 'I am also passionate about sharing my learning journey and insights as an instructor. This dual role not only accelerates my growth in the field but also allows me to contribute to the development of others. Together, lets work towards enhancing your digital presence and achieving your online ambitions with cutting-edge solutions' ]
+    },
+    {
+        title: "Backend Development",
+        category: "Backend",
+        description: ['Hello, I am Lawal Omeiza, a frontend developer and instructor from Nigeria, now exploring the world as a digital nomad and freelancing from various locations globally. My commitment is to elevate your business online by providing top-notch, industry-standard web solutions', 'My expertise spans HTML, CSS, JavaScript, and Git, with a keen focus on expanding my skills in ReactJS. As I continue to deepen my knowledge in ReactJS', 'I am also passionate about sharing my learning journey and insights as an instructor. This dual role not only accelerates my growth in the field but also allows me to contribute to the development of others. Together, lets work towards enhancing your digital presence and achieving your online ambitions with cutting-edge solutions' ]
+    },
+    {
+        title: "Digital Marketing",
+        category: "digital",
+        description: ['Lets ensure your business gets the best SEO right from the start. Straightforward SEO guidance is crucial in the initial phases of your website development. Having an expert SEO consultant collaborate with your developer and designer is key to a smooth website redesign or migration', ]
+    },
+]
